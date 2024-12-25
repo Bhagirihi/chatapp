2636340bf9fdac6cd1d191895fae0f3a801b156b
Dhruv Dave <tatvmasiproject@gmail.com>
2024-12-25 21:11:00 +0530

    Other option for Cookies
    Other option for Cookies

--- index.js
+++ index.js
@@ -18,35 +18,39 @@ let headers = {
 
 const getCookiesWithPuppeteer = async () => {
   try {
-    const browser = await puppeteer.launch({
-      headless: "new",
-      args: [
-        "--disable-setuid-sandbox",
-        "--no-sandbox",
-        "--single-process",
-        "--no-zygote",
-      ],
-      executablePath:
-        process.env.NODE_ENV == "production"
-          ? process.env.PUPPETEER_EXECUTABLE_PATH
-          : puppeteer.executablePath(),
-    });
-    const page = await browser.newPage();
-    await page.setUserAgent(
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0 Safari/537.36"
-    );
-    await page.goto("https://www.nseindia.com", {
-      waitUntil: "domcontentloaded",
+    const response = await axios.get("https://www.nseindia.com", {
+      withCredentials: true,
+      headers: {
+        "User-Agent": "Mozilla/5.0", // Helps avoid bot detection
+      },
     });
 
-    const cookies = await page.cookies();
-    await browser.close();
-
-    return cookies.map((cookie) => `${cookie.name}=${cookie.value}`).join("; ");
+    const cookies = response.headers["set-cookie"];
+    console.log("Cookies: ==>", cookies); // Array of cookies
+    return cookies;
   } catch (error) {
-    console.error("Error fetching cookies:", error.message);
-    throw new Error("Failed to retrieve cookies");
+    console.error("Error fetching cookies:", error);
   }
+  // try {
+  //   const browser = await puppeteer.launch({
+  //     headless: "new",
+  //   });
+  //   const page = await browser.newPage();
+  //   await page.setUserAgent(
+  //     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0 Safari/537.36"
+  //   );
+  //   await page.goto("https://www.nseindia.com", {
+  //     waitUntil: "domcontentloaded",
+  //   });
+
+  //   const cookies = await page.cookies();
+  //   await browser.close();
+
+  //   return cookies.map((cookie) => `${cookie.name}=${cookie.value}`).join("; ");
+  // } catch (error) {
+  //   console.error("Error fetching cookies:", error.message);
+  //   throw new Error("Failed to retrieve cookies");
+  // }
 };
 
 const fetchData = async (url) => {
