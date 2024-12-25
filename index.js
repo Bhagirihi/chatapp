dce1e8eed463597aaee1813d9941ba07c7e0f7bf
Dhruv Dave <tatvmasiproject@gmail.com>
2024-12-25 15:11:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,26 +16,26 @@ let headers = {
   Referer: "https://www.nseindia.com/",
 };
 
-const getCookiesWithPuppeteer = async () => {
-  try {
-    const browser = await puppeteer.launch({ headless: "new" });
-    const page = await browser.newPage();
-    await page.setUserAgent(
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0 Safari/537.36"
-    );
-    await page.goto("https://www.nseindia.com", {
-      waitUntil: "domcontentloaded",
-    });
+// const getCookiesWithPuppeteer = async () => {
+//   try {
+//     const browser = await puppeteer.launch({ headless: "new" });
+//     const page = await browser.newPage();
+//     await page.setUserAgent(
+//       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0 Safari/537.36"
+//     );
+//     await page.goto("https://www.nseindia.com", {
+//       waitUntil: "domcontentloaded",
+//     });
 
-    const cookies = await page.cookies();
-    await browser.close();
+//     const cookies = await page.cookies();
+//     await browser.close();
 
-    return cookies.map((cookie) => `${cookie.name}=${cookie.value}`).join("; ");
-  } catch (error) {
-    console.error("Error fetching cookies:", error.message);
-    throw new Error("Failed to retrieve cookies");
-  }
-};
+//     return cookies.map((cookie) => `${cookie.name}=${cookie.value}`).join("; ");
+//   } catch (error) {
+//     console.error("Error fetching cookies:", error.message);
+//     throw new Error("Failed to retrieve cookies");
+//   }
+// };
 
 const fetchData = async (url) => {
   try {
