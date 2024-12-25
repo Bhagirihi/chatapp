2ef4725483b96df86a284e6613dafacce8d8e3c7
Dhruv Dave <tatvmasiproject@gmail.com>
2024-12-25 15:50:01 +0530

    add docker related to PUPPETEER
    add docker related to PUPPETEER

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const puppeteer = require("puppeteer");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-
+require("dotenv").config();
 const PORT = process.env.PORTING || 4000;
 let headers = {
   "User-Agent":
@@ -16,26 +16,38 @@ let headers = {
   Referer: "https://www.nseindia.com/",
 };
 
-// const getCookiesWithPuppeteer = async () => {
-//   try {
-//     const browser = await puppeteer.launch({ headless: "new" });
-//     const page = await browser.newPage();
-//     await page.setUserAgent(
-//       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0 Safari/537.36"
-//     );
-//     await page.goto("https://www.nseindia.com", {
-//       waitUntil: "domcontentloaded",
-//     });
+const getCookiesWithPuppeteer = async () => {
+  try {
+    const browser = await puppeteer.launch({
+      headless: "new",
+      args: [
+        "--disable-setuid-sandbox",
+        "--no-sandbox",
+        "--single-process",
+        "--no-zygote",
+      ],
+      executablePath:
+        process.env.NODE_ENV == "production"
+          ? process.env.PUPPETEER_EXECUTABLE_PATH
+          : puppeteer.executablePath(),
+    });
+    const page = await browser.newPage();
+    await page.setUserAgent(
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0 Safari/537.36"
+    );
+    await page.goto("https://www.nseindia.com", {
+      waitUntil: "domcontentloaded",
+    });
 
-//     const cookies = await page.cookies();
-//     await browser.close();
+    const cookies = await page.cookies();
+    await browser.close();
 
-//     return cookies.map((cookie) => `${cookie.name}=${cookie.value}`).join("; ");
-//   } catch (error) {
-//     console.error("Error fetching cookies:", error.message);
-//     throw new Error("Failed to retrieve cookies");
-//   }
-// };
+    return cookies.map((cookie) => `${cookie.name}=${cookie.value}`).join("; ");
+  } catch (error) {
+    console.error("Error fetching cookies:", error.message);
+    throw new Error("Failed to retrieve cookies");
+  }
+};
 
 const fetchData = async (url) => {
   try {
@@ -94,7 +106,7 @@ io.on("connection", async (socket) => {
     "Cache-Control": "no-cache",
     Connection: "keep-alive",
   };
-  console.log("header", headers.length > 0);
+
   socket.emit("loader", false);
 
   socket.on("chat message", (msg) => {
